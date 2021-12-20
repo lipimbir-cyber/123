3c61a37b64fda7dfe6354cd6681170cfa7b62577
lipimbir-cyber <80145486+lipimbir-cyber@users.noreply.github.com>
2021-12-20 08:26:20 +0300

    123
    123

--- 123/Form1.cs
+++ 123/Form1.cs
@@ -28,7 +28,6 @@ namespace _123
             zadachi zad = new zadachi();
             zad.ShowDialog();
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             otdel otd = new otdel();
